b7e35ab0d5ad3c0d081ad3cc5b23091cc73717b1
elianaSpe <speranza.eliana.e@gmail.com>
2026-06-20 23:19:23 -0300

    3.3 Completar el formulario con datos válidos

--- cypress/e2e/myTestRestfulBooker.cy.js
+++ cypress/e2e/myTestRestfulBooker.cy.js
@@ -12,6 +12,31 @@ describe('3.1 Reserva exitosa como usuario invitado', () => {
       cy.get('.row.g-4').should('be.visible')
       cy.get('.row.g-4').should('have.length',3)
      })
+
+
+
+     it('Completar el formulario con datos válidos (nombre, apellido, email, teléfono y fechas)', () => {
+    cy.contains('Book now').first().click()
+
+cy.url().should('include', '/reservation/')
+
+ cy.get('#doReservation').should('be.visible').click()
+
+
+cy.get('input[name="firstname"]')
+  .should('be.visible')
+
+      cy.fixture('reserva').then((datos) => {
+
+    cy.get('input[name="firstname"]').type(datos.nombre)
+    cy.get('input[name="lastname"]').type(datos.apellido)
+    cy.get('input[name="email"]').type(datos.email)
+    cy.get('input[name="phone"]').type(datos.telefono)
+    
+     })
+     })
   })
+
+  
 
  
