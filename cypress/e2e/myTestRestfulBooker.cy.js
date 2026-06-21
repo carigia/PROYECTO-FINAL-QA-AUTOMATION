370b69eaf52ecd0439a81c5a1b0781d722f01ba5
carigia <110430160+carigia@users.noreply.github.com>
2026-06-21 10:05:33 -0300

    Update myTestRestfulBooker.cy.js

--- cypress/e2e/myTestRestfulBooker.cy.js
+++ cypress/e2e/myTestRestfulBooker.cy.js
@@ -6,12 +6,16 @@ describe('3.1 Reserva exitosa como usuario invitado', () => {
       cy.visit('https://automationintesting.online')
     })
     it('debe mostrar habitaciones disponibles en la página principal', () => {
-     
-      // Verifica el título de la sección de habitaciones
+     // Verifica el título de la sección de habitaciones
       cy.contains('Our Rooms').should('be.visible')
       cy.get('.row.g-4').should('be.visible')
       cy.get('.row.g-4').should('have.length',3)
      })
+   it(' Seleccionar habitación Single y navegar a la reserva', () => {
+      cy.get(':nth-child(2) > .card > .card-footer > .btn').should('be.visible')
+      cy.get(':nth-child(2) > .card > .card-footer > .btn').click()  
+      cy.get('[id="doReservation"]').click()
   })
+})
 
  
