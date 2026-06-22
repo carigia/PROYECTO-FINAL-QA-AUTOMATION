d92818f53342cb9058eb4fced8e615f6f307d77f
carigia <110430160+carigia@users.noreply.github.com>
2026-06-22 09:37:16 -0300

    Update myTestRestfulBooker.cy.js

--- cypress/e2e/myTestRestfulBooker.cy.js
+++ cypress/e2e/myTestRestfulBooker.cy.js
@@ -1,16 +1,44 @@
-describe("3.1 Reserva exitosa como usuario invitado", () => {
-  Cypress.on("uncaught:exception", (err, runnable) => {
-    return false;
+it("3.1.1 debe mostrar habitaciones disponibles en la página principal", () => {
+    // Verifica el título de la sección de habitaciones
+    cy.contains("Our Rooms").should("be.visible");
+    cy.get(".row.g-4").should("be.visible");
+    cy.get(".row.g-4").should("have.length", 3);
   });
-  beforeEach(() => {
-    cy.visit('https://automationintesting.online')
-  })
-  it('debe mostrar habitaciones disponibles en la página principal', () => {
-    cy.contains('Our Rooms').should('be.visible')
-    cy.get('.row.g-4').should('be.visible')
-    cy.get('.row.g-4').should('have.length', 3)
-  })
-})
+  it("3.1.2 y 3.1.3 Completar el formulario con datos válidos (nombre, apellido, email, teléfono y fechas)", () => {
+    //Busco el primer elemento que contenga book now y hago click
+    cy.contains("Book now").first().click();
+    //verifico que vaya a una url de reservación
+    cy.url().should("include", "/reservation/");
+    // Reviso que esté visible el botón para reservar y hago click
+    cy.get("#doReservation").should("be.visible").click();
+    // verifico que se vea el input de firstname que es el primero del formulario
+    cy.get('input[name="firstname"]').should("be.visible");
+
+    cy.fixture("reserva").then((datos) => {
+      cy.completarFormularioReserva(datos);
+    });
+  });
+  it("3.1.4 Confirmar la reserva y validar que el mensaje de éxito aparece en pantalla", () => {
+    cy.contains("Book now").first().click();
+    cy.url().should("include", "/reservation/");
+    cy.get("#doReservation").should("be.visible").click();
+    cy.get('input[name="firstname"]').should("be.visible");
+
+    //Busco comando que rellena reserva
+    cy.fixture("reserva").then((datos) => {
+      cy.completarFormularioReserva(datos);
+
+      cy.get(".btn-primary.w-100.mb-3")
+        .should("contain.text", "Reserve")
+        .click();
+
+      // cy.contains('Booking Confirmed').should('be.visible')
+
+      //cy.contains('Your booking has been confirmed for the following dates:')
+      //.should('be.visible').and('include.text', `${datos.checkin} - ${datos.checkout}`)
+    });
+  });
+});
 
 describe('3.2 Validaciones del formulario de reserva', () => {
   Cypress.on('uncaught:exception', () => false)
@@ -86,7 +114,6 @@ describe("3.2 (Adicional) Completar formulario", () => {
     });
   });
 });
-
 describe("3.3 Formulario de contacto", () => {
   Cypress.on("uncaught:exception", () => false);
   beforeEach(() => {
