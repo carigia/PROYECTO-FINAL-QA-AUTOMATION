3d6e3dead241aa77b60b82a8cac60e6032d8bf94
Carina <cgiacoletti@hotmail.com>
2026-06-22 09:12:45 -0300

    Agrego cambios

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-
+  video: true,
+  screenshotsFolder: "cypress/screenshots",
+  videosFolder: "cypress/videos",
   e2e: {
-    setupNodeEvents(on, config) {
-      
-      // implement node event listeners here
-        
-        
-    },
+    setupNodeEvents(on, config) {},
   },
 });
